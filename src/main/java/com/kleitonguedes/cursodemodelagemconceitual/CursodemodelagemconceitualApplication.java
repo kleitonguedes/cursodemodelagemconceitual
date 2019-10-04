187ef38c04faa6a0e3e7eea10350ffb1c9269bc2
Kleiton Guedes <kleiton.guedess@gmail.com>
2019-10-04 12:01:11 -0300

    Projeto criado

---
+++ src/main/java/com/kleitonguedes/cursodemodelagemconceitual/CursodemodelagemconceitualApplication.java
@@ -0,0 +1,13 @@
+package com.kleitonguedes.cursodemodelagemconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursodemodelagemconceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursodemodelagemconceitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kleitonguedes/cursodemodelagemconceitual/CursodemodelagemconceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleitonguedes.cursodemodelagemconceitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursodemodelagemconceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
