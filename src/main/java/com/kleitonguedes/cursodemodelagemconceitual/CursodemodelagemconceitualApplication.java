bb5cb4fbcb64729d2feed8c30bbf8c3a03b18972
Kleiton Guedes <kleiton.guedess@gmail.com>
2019-10-04 13:36:32 -0300

    Criando operação de instanciação

--- src/main/java/com/kleitonguedes/cursodemodelagemconceitual/CursodemodelagemconceitualApplication.java
+++ src/main/java/com/kleitonguedes/cursodemodelagemconceitual/CursodemodelagemconceitualApplication.java
@@ -1,13 +1,34 @@
 package com.kleitonguedes.cursodemodelagemconceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursodemodelagemconceitualApplication {
+import com.kleitonguedes.cursodemodelagemconceitual.domain.Categoria;
+import com.kleitonguedes.cursodemodelagemconceitual.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursodemodelagemconceitualApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursodemodelagemconceitualApplication.class, args);
+	}
+	
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
